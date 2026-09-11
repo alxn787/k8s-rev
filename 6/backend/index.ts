e45f2d937f703332023267073638f433a0b03cff
alxn787 <alenabraham787@gmail.com>
2026-09-12 01:23:20 +0530

    secrets

---
+++ 6/backend/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: process.env.DATABASE_URL, port: process.env.PORT });
+});
+
+console.log(process.env.DATABASE_URL);
+
+console.log(process.env.PORT);
+
+app.listen(process.env.PORT);
